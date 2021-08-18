9dee37e91e4c1fe1b6d3aa93ae40aba1e7686963
benjabarreiro <barreirobenjamin@gmail.com>
2021-08-18 14:50:08 -0300

    mini array productos y array carousel

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import './App.css';
 import Header from "./components/Header/Header";
-import SideMenu from "./components/SideMenu/SideMenu";
+//import SideMenu from "./components/SideMenu/SideMenu";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
 import Contact from "./pages/Contact/Contact";
 import Category from "./pages/Category/Category";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <SideMenu />
       <Switch>
         <Route path="/" component={Home} />
         <Route path="/category/:categoryName/:subCategoryName?" component={Category} />
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from "react";
+import arrs from "../../data";
+import Carousel from 'react-bootstrap/Carousel'
+
+const {data, carousel} = arrs;
 
 export default function Home() {
   const [products, setProducts] = useState([]);
@@ -13,15 +17,43 @@ export default function Home() {
   }, []);
 
   const fetchProducts = async () => {
-    const res = await fetch();
+    /* const res = await fetch();
     const data = await res.json();
 
-    return data;
+    return data; */
+    const productsFromDB = data;
+
+    return productsFromDB;
   };
 
   return (
-    <div>
-      <h1>home</h1>
-    </div>
+    <>
+      <Carousel>
+        {carousel.map(element => <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={element}
+            alt=""
+          />
+        </Carousel.Item>)}
+      </Carousel>
+      {products.map((element) => {
+        return (
+          <div>
+            <img className="img-prod" src={element.img} alt="" />
+            <a target="_blank" rel="noreferrer" href={element.url}>
+              <h5>{element.title}</h5>
+            </a>
+            <p>{element.description}</p>
+            <p>
+              {element.price} <span>{element.savings}</span>
+            </p>
+            <a target="_blank" rel="noreferrer" href={element.url}>
+              <button>Ir a choyakos</button>
+            </a>
+          </div>
+        );
+      })}
+    </>
   );
 }
